bf52a5e52df520341b6d35429bb140b9b36e2751
liezsm <liez.dev@gmail.com>
2022-03-30 16:52:28 +0800

    project finished

--- src/airbnb/components/CardItem.js
+++ src/airbnb/components/CardItem.js
@@ -1,13 +1,36 @@
 import React from "react";
 
-export default function CardItem() {
+import star from "../img/star.png";
+
+export default function CardItem({
+  data: { img, button, rate, place, title, amt },
+}) {
+  const style = {
+    display: button ? "block" : "none",
+  };
   return (
     <article className='card_item'>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat nobis
-        quasi unde quidem veritatis? Quos inventore minima ipsa animi
-        asperiores.
-      </p>
+      <div className='card_img'>
+        <img src={img} alt='' className='card_img-photo' />
+
+        <a href='#' className='card_img_tag' style={style}>
+          {button}
+        </a>
+      </div>
+
+      <div className='card_desc'>
+        <div className='card_desc_rating'>
+          <img src={star} alt='' />
+          <p className='card_desc_rating_rate'>
+            {rate}
+            <span id='place'> {place}</span>
+          </p>
+        </div>
+        <p className='card_desc_rating_title'>{title}</p>
+        <div className='card_desc_rating_amt'>
+          <span id='amount'>{amt}</span> person
+        </div>
+      </div>
     </article>
   );
 }
--- src/airbnb/components/Cards.js
+++ src/airbnb/components/Cards.js
@@ -1,14 +1,42 @@
 import React from "react";
 
+import swim from "../img/swimmer.png";
+import wed from "../img/wedding-photography.png";
+import bike from "../img/mountain-bike.png";
+
 import CardItem from "./CardItem";
 export default function Cards() {
+  const items = [
+    {
+      img: swim,
+      button: "SOLD OUT",
+      rate: "5.0",
+      place: "(6) USA",
+      title: "Life lessons with Katie Zaferes",
+      amt: "From $136 / ",
+    },
+    {
+      img: wed,
+      button: "ONLINE",
+      rate: "5.0",
+      place: "(30) USA",
+      title: "Learn wedding photography",
+      amt: "From $125 / ",
+    },
+    {
+      img: bike,
+      button: "",
+      rate: "4.8",
+      place: "(2) USA",
+      title: "Group Mountain Biking",
+      amt: "From $50/",
+    },
+  ];
   return (
     <section className='cards'>
-      test
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, amet!
-      </p>
-      <CardItem />
+      {items.map((i, index) => (
+        <CardItem key={index} data={i} />
+      ))}
     </section>
   );
 }
